d3dc89adc601fbccf851442c2dc359531cd4cab4
Elvis Nwogu <nwoguelvis92@gmail.com>
2026-01-20 14:54:39 +0000

    Fix case sensitivity in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import {Routes, Route, useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./components/Product";
 import Footer from "./components/Footer";
-import Workshop from "./components/WorkShop";
+import Workshop from "./components/Workshop"; // FIX 1: Changed 'WorkShop' to 'Workshop'
 
 function App() {
   const location = useLocation();
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/products" element={<Products/>} />
-        <Route path="/Workshop" element={<Workshop/>} />
-      </Routes>
-    </AnimatePresence>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          {/* FIX 2: Changed path from '/Workshop' to '/workshop' to match your button */}
+          <Route path="/workshop" element={<Workshop />} /> 
+        </Routes>
+      </AnimatePresence>
 
-    <Footer/>
+      <Footer />
     </>
   );
 }
